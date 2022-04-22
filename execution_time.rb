18d7d19059bb260fcdb1922ddf57f71cd565d6f5
Eric Balfour <egbalfour@gmail.com>
2022-04-22 18:13:41 -0400

    format changes

--- execution_time.rb
+++ execution_time.rb
@@ -1,46 +1,46 @@
 # #Phase I
 
-# def my_min(array)
-#     array.sort.first
-# end
+def my_min(array)
+    array.sort.first
+end
 
-# #phase IIß
-# def my_min(array)
-#     array.inject{|ele1, ele2|  ele1 < ele2 ? ele1 : ele2}
+# #phase II
+def my_min(array)
+    array.inject{|ele1, ele2|  ele1 < ele2 ? ele1 : ele2}
 
-# end
-# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-# p my_min(list)
-
-
+end
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+p my_min(list)
 
 
-# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-# p my_min(list)
 
-#Phase 1
 
-# def largest_contiguous_subsum(array)
-#     new_array =[]
-#     sum =0
-#     (0...array.length).each do |si|
-#         (si...array.length).each do |ei|
-#             new_array << array[si..ei]
-#         end
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+p my_min(list)
+
+Phase 1
+
+def largest_contiguous_subsum(array)
+    new_array =[]
+    sum =0
+    (0...array.length).each do |si|
+        (si...array.length).each do |ei|
+            new_array << array[si..ei]
+        end
     
-#     end
-#     new_array.sort_by! {|sub_arr| sub_arr.sum}
-#     new_array.last.sum
-# end
+    end
+    new_array.sort_by! {|sub_arr| sub_arr.sum}
+    new_array.last.sum
+end
 
-# list = [5, 3, -7]
-# p largest_contiguous_subsum(list) # => 8
+list = [5, 3, -7]
+p largest_contiguous_subsum(list) # => 8
 
-# list2 = [2, 3, -6, 7, -6, 7]
-# p largest_contiguous_subsum(list2) # => 8 (from [7, -6, 7])
+list2 = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum(list2) # => 8 (from [7, -6, 7])
 
-# list = [-5, -1, -3]
-# p largest_contiguous_subsum(list) # => -1 (from [-1])
+list = [-5, -1, -3]
+p largest_contiguous_subsum(list) # => -1 (from [-1])
 
 #Phase II
 
